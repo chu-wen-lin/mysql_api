64369cead3a70528b8bab719793dfc948b23fba5
Chu-Wen Lin [林筑玟] <chuwenlin@eland.com.tw>
2021-07-30 17:53:29 +0800

    Modify keywords input field. Now users hit 'Add string item' button once to add a keyword. That is, four hits for four keywords for sure!.

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 
 import uvicorn
 from datetime import date
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 from object import schema
 from typing import Optional, List
 from utilities import tools
@@ -14,7 +14,7 @@ tags_metadata = [
         "description": "Type s_id, s_area_id, start time, end time and keywords to filter posts. "
                        "'s_id' represents forum sites. 's_area_id' represents forum boards. "
                        "When start_time and end_time are filled, you'll get posts whose post time are in [start, end]. "
-                       "If you are willing to select posts with multiple words, use comma to separate keywords. "                       
+                       "If you are willing to select posts with multiple words, hit 'Add string item' to add more keywords. "                       
                        "If there is no condition specified, we'll return the newest 100 posts."
 
     }
@@ -37,11 +37,11 @@ async def select_posts(s_id: Optional[str] = None,
                        s_area_id: Optional[str] = None,
                        start_time: Optional[date] = None,
                        end_time: Optional[date] = None,
-                       keywords: str = None,
+                       keywords: Optional[List[str]] = Query(None),
                        limit: int = 100,
                        offset: int = 0):
+
     if keywords:
-        keywords = keywords.split(',')
         keywords = [keyword.strip(' ') for keyword in keywords]
         keywords = '%'.join(keywords)
 
