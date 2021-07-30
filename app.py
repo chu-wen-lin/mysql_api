1a1948dee9ef75624d1f157861138d52718b0f8c
Chu-Wen Lin [林筑玟] <chuwenlin@eland.com.tw>
2021-07-30 11:56:18 +0800

    Bug fixed. Not neccessary for users to specify conditions during query.

--- app.py
+++ app.py
@@ -1,19 +1,20 @@
 # TODO: build FastAPI service
 
 import uvicorn
+from datetime import date
 from fastapi import FastAPI
 from object import schema
 from typing import Optional, List
 from utilities import tools
 
-# from utilities.database import SessionLocal
 
 tags_metadata = [
     {
         "name": "selection",
         "description": "Type a s_id, s_area_id, start and end datetime to filter posts. "
-                       "e.g. s_id refers to forum sites. s_area_id refers to forum boards."
-                       "When start_time and end_time are filled, you'll get posts whose post time are in [start, end]."
+                       "'s_id' represents forum sites. 's_area_id' represents forum boards. "
+                       "When start_time and end_time are filled, you'll get posts whose post time are in [start, end]. "
+                       "If there is no condition specified, MYSQL API will return newest 100 posts."
 
     }
 ]
@@ -33,38 +34,13 @@ async def home():
 @app.get('/posts', tags=['selection'], response_model=List[schema.TypeHintOut])
 async def select_posts(s_id: Optional[str] = None,
                        s_area_id: Optional[str] = None,
-                       start_time: Optional[str] = None,
-                       end_time: Optional[str] = None,
-                       limit: Optional[int] = None,
-                       offset: Optional[int] = None):
-    return tools.select_posts(s_id=s_id, s_area_id=s_area_id, start_time=start_time, end_time=end_time,
-                              limit=limit, offset=offset)
+                       start_time: Optional[date] = None,
+                       end_time: Optional[date] = None,
+                       limit: int = 100,
+                       offset: int = 0):
 
-
-# @app.get('/posts/post-time', tags=['post_time'], response_model=List[schemas.TypeHintOut])
-# async def select_posts_by_post_time(start_time: Optional[str] = '2019-09-01 00:00:00',
-#                                     end_time: Optional[str] = '2019-09-30 23:59:59',
-#                                     limit: int = 1000,
-#                                     offset: int = 0):
-#     def string_to_datetime(datetime_string: str):
-#         try:
-#             if len(datetime_string.split()) == 2:
-#                 datetime_obj = datetime.strptime(datetime_string, '%Y-%m-%d %H:%M:%S')
-#                 return datetime.strftime(datetime_obj, '%Y-%m-%d %H:%M:%S')
-#             elif len(datetime_string.split()) == 1:
-#                 datetime_obj = datetime.strptime(datetime_string + ' 23:59:59', '%Y-%m-%d %H:%M:%S')
-#                 return datetime.strftime(datetime_obj, '%Y-%m-%d %H:%M:%S')
-#         except:
-#             return False
-#
-#     start_time = string_to_datetime(start_time)
-#     end_time = string_to_datetime(end_time)
-#     if start_time and end_time:
-#         return tools.select_posts(s_id=s_id, s_area_id=s_area_id, start_time=start_time, end_time=end_time,
-#                                   limit=limit, offset=offset)
-#     else:
-#         return {'Status': 'Error Occurred'}
-
+    return tools.select_posts(s_id=s_id, s_area_id=s_area_id, start_time=start_time,
+                              end_time=end_time, limit=limit, offset=offset)
 
 if __name__ == "__main__":
     uvicorn.run("app:app", host="127.0.0.1", port=8000, reload=True, debug=True)
--- utilities/tools.py
+++ utilities/tools.py
@@ -4,38 +4,36 @@ import pymysql
 from object.database import get_db
 from object.schema import TypeHintOut as Schema
 
-# from fastapi import Depends
-# from sqlalchemy.orm import Session
-# from sqlalchemy import and_, desc
-# from models import Model
 
-
-def select_posts(limit: int = 1000,
-                 offset: int = 0,
+def select_posts(limit,
+                 offset,
                  **kwargs):
 
     sub_query = None
     for key in kwargs.keys():  # retrieve parameters excluding limit and offset
-        if not sub_query:
-            if key == 'start_time':
-                sub_query = f" WHERE `post_time` >= '{kwargs[key]}'"
-            elif key == 'end_time':
-                sub_query = f" WHERE `post_time` <= '{kwargs[key]}'"
+        if kwargs[key]:
+            if not sub_query:
+                if key == 'start_time':
+                    sub_query = f" WHERE `post_time` >= '{kwargs[key]}'"
+                elif key == 'end_time':
+                    sub_query = f" WHERE `post_time` <= '{kwargs[key]}'"
+                else:
+                    sub_query = f" WHERE `{key}` = '{kwargs[key]}'"
             else:
-                sub_query = f" WHERE `{key}` = '{kwargs[key]}'"
-        else:
-            if key == 'start_time':
-                sub_query += f" AND `post_time` >= '{kwargs[key]}'"
-            elif key == 'end_time':
-                sub_query += f" AND `post_time` <= '{kwargs[key]}'"
-            else:
-                sub_query += f" AND `{key}` = '{kwargs[key]}'"
+                if key == 'start_time':
+                    sub_query += f" AND `post_time` >= '{kwargs[key]}'"
+                elif key == 'end_time':
+                    sub_query += f" AND `post_time` <= '{kwargs[key]}'"
+                else:
+                    sub_query += f" AND `{key}` = '{kwargs[key]}'"
 
     with get_db() as connection:
         with connection.cursor(pymysql.cursors.DictCursor) as cursor:
-
             columns = ','.join(Schema.__dict__.get("__fields__").keys())
-            query = f"SELECT {columns} FROM `ts_page_content` {sub_query} ORDER BY `post_time` DESC limit {limit} offset {offset}"
+            if sub_query:
+                query = f"SELECT {columns} FROM `ts_page_content` {sub_query} ORDER BY `post_time` DESC limit {limit} offset {offset}"
+            else:
+                query = f"SELECT {columns} FROM `ts_page_content` ORDER BY `post_time` DESC limit {limit} offset {offset}"
             cursor.execute(query)
 
             result = cursor.fetchone()
@@ -44,9 +42,3 @@ def select_posts(limit: int = 1000,
                 result = cursor.fetchone()
 
             connection.commit()
-
-# class CommonQueryParams:
-#     def __init__(self, db: Session, skip: int = 0, limit: int = 10000):
-#         self.db = db
-#         self.skip = skip
-#         self.limit = limit
